840856c97b50dfc9cf04ce2d5c73d193f1849d1d
Sagar Chawla <sagarchawla83@gmail.com>
2023-12-24 13:41:47 +0530

    mobile filter ui

--- src/components/Filter/FilterMoblie.js
+++ src/components/Filter/FilterMoblie.js
@@ -1,28 +1,44 @@
-import React, { useState } from 'react'
-import styles from './Filter.module.css'
-import { Filter, X } from 'react-feather'
+import React, { useState } from 'react';
+import styles from './Filter.module.css';
+import { Filter, X } from 'react-feather';
 import { Filters } from './Filters';
 
+export const FilterMoblie = ({
+  setPage,
+  filteredStatus,
+  filteredGender,
+  filteredSpecies,
+  setFilteredGender,
+  setFilteredStatus,
+  setFilteredSpecies,
+}) => {
+  const [open, setOpen] = useState(false);
 
-export const FilterMoblie = ({ setPage, filteredStatus, filteredGender, filteredSpecies, setFilteredGender , setFilteredStatus , setFilteredSpecies }) => {
-    const [open, setOpen] = useState(false);
-    return (
-        <div className={styles.mobileFiltersContainer}>
-            <div onClick={() => setOpen(!open)} className={styles.filterIcon} >
-                { open ? <X color='grey'/> : <Filter color='grey'/> }
-            </div>
-            <div style={{display: open ? 'block' : 'none'}}>
-                    <Filters 
-                        setPage={setPage}
-                        filteredStatus={filteredStatus} 
-                        filteredGender={filteredGender} 
-                        filteredSpecies={filteredSpecies} 
-                        setFilteredGender={setFilteredGender} 
-                        setFilteredStatus={setFilteredStatus} 
-                        setFilteredSpecies={setFilteredSpecies} 
-                    />
-             
-            </div>
+  const toggleFilter = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className={styles.mobileFiltersContainer}>
+      <div onClick={toggleFilter} className={styles.filterIcon}>
+        {open ? <X color='grey' /> : <Filter color='grey' />}
+      </div>
+      {open && (
+        <div className={styles.mFilter}>
+          <div onClick={toggleFilter} className={styles.filterIcon}>
+            {open ? <X color='grey' /> : <Filter color='grey' />}
+          </div>
+          <Filters
+            setPage={setPage}
+            filteredStatus={filteredStatus}
+            filteredGender={filteredGender}
+            filteredSpecies={filteredSpecies}
+            setFilteredGender={setFilteredGender}
+            setFilteredStatus={setFilteredStatus}
+            setFilteredSpecies={setFilteredSpecies}
+          />
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
